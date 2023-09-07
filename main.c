e462d9439b9c003a18065eca3204f515d6cc4ab0
root <Nicolaslegoat>
2023-09-07 01:54:04 -0700

    betty pb

--- main.c
+++ main.c
@@ -1,28 +1,28 @@
 #include "monty.h"
 monty_t monty = {NULL, NULL, NULL, 0};
 /**
-* main - monty code interpreter
-* @argc: number of arguments
-* @argv: monty file location
-* Return: 0 on success
-*/
-int main (int argc, char *argv[])
+ * main - monty code interpreter
+ * @argc: number of arguments
+ * @argv: monty file location
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
 {
-    char *content;
-    FILE *file;
-    size_t size = 0;
-    ssize_t read_line = 1;
-    stack_t *stack = NULL;
-    unsigned int counter = 0;
+	char *content;
+	FILE *file;
+	size_t size = 0;
+	ssize_t read_line = 1;
+	stack_t *stack = NULL;
+	unsigned int counter = 0;
 
-    if (argc != 2)
-    {
-        printf("USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
-    file = fopen(argv[1], "r");
-    monty.file = file;
-    if (!file)
+	if (argc != 2)
+	{
+		printf("USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	file = fopen(argv[1], "r");
+	monty.file = file;
+	if (!file)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
@@ -41,6 +41,6 @@ int main (int argc, char *argv[])
 	}
 	free_stack(stack);
 	fclose(file);
-return (0);
+	return (0);
 }
 
