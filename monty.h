23420bd0f0b88c3899ac4e53d78326c573b3a91e
root <Nicolaslegoat>
2023-09-08 14:29:00 -0700

    clear proto

--- monty.h
+++ monty.h
@@ -60,6 +60,5 @@ void addqueue(stack_t **head, int n);
 void f_pint(stack_t **head, unsigned int counter);
 void f_pop(stack_t **head, unsigned int counter);
 void f_nop(stack_t **head, unsigned int counter);
-void f_swap(stack_t **head, unsigned int counter);
 
 #endif
